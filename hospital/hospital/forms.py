f01c95be509438cc97cdc5a28fdf884c15fd7a4f
vyvy072005 <94981979+vyvy072005@users.noreply.github.com>
2025-05-17 23:06:03 +0400

    Delete hospital directory

--- hospital/hospital/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Patient
-
-class PatientForm(forms.ModelForm):
-    class Meta:
-        model = Patient
-        fields = '__all__'  # Или перечислите конкретные поля
